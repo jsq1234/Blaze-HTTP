2cbf94638afbe8118556c3380d724bf0858c0de6
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-10 18:06:48 +0530

    updated

--- src/http.c
+++ src/http.c
@@ -1,37 +1,61 @@
 #include "../include/http.h"
+#include "../include/utils.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 http_t request;
-int parse_request(const char *msg, http_t *request) {
+int parse_request(const char *msg, size_t msg_len, http_t *request) {
   const char *ptr = msg;
+  size_t left = msg_len;
+  
   if (strncmp(ptr, "GET", strlen("GET")) == 0) {
     strcpy(request->method, "GET");
-    ptr += 4;
+    MOVE(ptr,left,4)
   } else {
     return -1;
   }
   const char *itr = ptr;
-
-  while (*itr != ' ')
-    itr++;
+  TRAVERSE_TILL(itr, ' ')
   int len = itr - ptr;
 
   strncpy(request->url, ptr, len);
-
   request->url[len] = '\0';
 
-  ptr = itr + 1;
+  MOVE(ptr,left,len+1) // +1 for the space 
   itr = ptr;
 
-  while (*itr != '\r')
-    itr++;
-
+  TRAVERSE_TILL(itr, '\r')
   len = itr - ptr;
 
   strncpy(request->version, ptr, len);
-
   request->version[len] = '\0';
 
+  return 0;
+  // to be implemented in the future
+  // for now, only parses the first line, i.e the request line
+
+  MOVE(ptr,left,len)
+  // if we have reached EOF (\r\n\r\n) the return 
+  CHECK_EOF(left,ptr)
+  //move by 2 to go the next line 
+
+  return 0;
+}
+
+int generate_response(http_t* request, char* response){
+  const char* filepath = "";
+  if( strcmp(request->url, "/") == 0 ){
+    filepath = "index.html";
+  }else{
+    filepath = &request->url[1];
+  }
+  FILE* fptr = fopen(filepath,"r");
+  if( fptr == NULL ){
+    handle_error(request, response, FNOTFOUND);
+  }
+  
+  
+   
   return 0;
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
   const char *msg = "GET /folder/sub_folder/poggers.js HTTP/1.1\r\n\r\n";
   memset(&request, 0, sizeof(request));
-  parse_request(msg, &request);
+  parse_request(msg,strlen(msg),&request);
   printf("method: %s\n", request.method);
   printf("url: %s\n", request.url);
   printf("version: %s\n", request.version);

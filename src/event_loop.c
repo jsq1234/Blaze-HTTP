7050e494ae6cc286448a29e43436534242b16ea7
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-24 12:48:16 +0530

    added a function

--- src/event_loop.c
+++ src/event_loop.c
@@ -1,19 +1,68 @@
 #include "event_loop.h"
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-int bz_epoll_init(bz_epoll_t* ep, size_t size){
-    ep->epollfd = epoll_create1(0);
-    if( ep->epollfd == -1 ){
-        perror("epoll_create\n");
+static int bz_epoll_init(event_loop_t *event_loop, size_t size){
+    bz_epoll_t* p = malloc(sizeof(*p));
+
+    if( p == NULL ){
+        perror("Couldn't create bz_epoll_t\n");
         return -1;
     }
 
-    ep->events = malloc(sizeof(struct epoll_event)*size);
-
-    if( ep->events == NULL ){
-        perror("Couldn't allocate memory for ep->events\n");
+    if( (p->epollfd = epoll_create1(0)) < 0 ){
+        perror("epoll_create1\n");
         return -1;
     }
+
+    p->events = malloc(sizeof(struct epoll_event)*size);
+
+    if( p->events == NULL ){
+        perror("Couldn't allocate bz_epoll_t.eventsn\n");
+        free(p);
+        return -1;
+    }
+
+    event_loop->epoll_data_size = size;
+
+    event_loop->epoll_data = p;
 
     return 0;
 }
 
+event_loop_t* bz_create_event_loop(size_t size){
+    event_loop_t* event_loop = malloc(sizeof(*event_loop));
+    
+    if( bz_epoll_init(event_loop, size) < 0 ){
+        free(event_loop);
+        return NULL;
+    }
+
+    event_loop->flags = 0;
+    event_loop->maxfd = size;
+
+    return event_loop;
+}
+
+void bz_add_event(event_loop_t *event_loop, int fd, int flags){
+    int efd = event_loop->epoll_data->epollfd;
+
+    struct epoll_event e = {0};
+
+    e.data.fd = fd;
+    e.events = flags;
+
+    int op = flags == BZ_NONE ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+
+    if( epoll_ctl(efd,op,fd,&e) < 0 ){
+        // if the operation failed, close the file descriptor
+        close(fd);
+        return ;
+    }
+
+}
+
+void bz_remove_event(event_loop_t* event_loop, int fd, int flags){
+    
+}
--- src/event_loop.h
+++ src/event_loop.h
@@ -5,23 +5,38 @@
 #include <stdlib.h>
 #include <sys/epoll.h>
 
+#define BZ_NONE 1<<0
+#define BZ_READABLE 1<<1
+#define BZ_WRITEABLE 1<<2
+
 struct event_loop_ds;
+
 typedef struct event_loop_ds event_loop_t;
 
-typedef void (*event_handler)(event_loop_t* event_loop);
+typedef void (*event_handler)(event_loop_t* event_loop, int fd, );
 
 typedef struct epoll_struct{
     int epollfd;
     struct epoll_event* events;
 } bz_epoll_t;
 
+typedef struct event_action_ds {
+    void (*accept_connection)(event_loop_t* event_loop,int fd);
+    void (*delete_connection)(event_loop_t* event_loop,int fd);
+    void (*write_message)(event_loop_t* event_loop, int fd);
+    void (*read_message)(event_loop_t* event_loop, int fd);
+} event_action_t;
+
 struct event_loop_ds{
     bz_epoll_t* epoll_data;
     int epoll_data_size;
     int maxfd;
     int flags;
+    event_action_t actions;
+};
 
-} event_loop_t;
 
-
+event_loop_t* bz_create_event_loop(size_t size);
+void bz_add_event(event_loop_t* event_loop, int op, int flags);
+void bz_delete_event(event_loop_t* event_loop, int op, int flags);
 #endif

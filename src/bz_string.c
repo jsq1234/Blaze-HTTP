5a3a9aa39e6c4ede62d73be9229b96acc08a4819
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-27 23:27:37 +0530

    just an irrelevant update

--- src/bz_string.c
+++ src/bz_string.c
@@ -1,58 +1,60 @@
 #include <assert.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
-typedef struct bz_string{
-    u_char* ptr;
-    size_t len;
-    size_t capacity;    
+typedef struct bz_string {
+	u_char *ptr;
+	size_t len;
+	size_t capacity;
 } bz_str_t;
 
+bz_str_t bz_string(const char *str) {
 
-bz_str_t bz_string(const char* str){
+	bz_str_t string;
 
-    bz_str_t string;
-    
-    string.len = strlen(str);
-    string.capacity = 2*string.len;
+	string.len = strlen(str);
+	string.capacity = 2 * string.len;
 
-    string.ptr = malloc(sizeof(*str)*string.capacity);
+	string.ptr = malloc(sizeof(*str) * string.capacity);
 
-    memcpy(string.ptr,str,string.len);
+	memcpy(string.ptr, str, string.len);
 
-    *(string.ptr + string.len) = '\0';
+	*(string.ptr + string.len) = '\0';
 
-    return string;
+	return string;
 }
 
+int bz_str_append(bz_str_t *str, const char *c_str) {
 
-int bz_str_append(bz_str_t* str, const char* c_str){
+	assert(c_str != NULL && "Null string passed to append.");
 
-    assert(c_str != NULL && "Null string passed to append.");
+	if (str->len + strlen(c_str) + 1 > str->capacity) {
 
-    if( str->len + strlen(c_str) + 1 > str->capacity ){
-        
-        str->ptr = realloc(str->ptr,str->capacity*2);
-        if(!str->ptr){
-            fprintf(stderr,"couldn't allocate more memory.\n");
-            return -1;
+		str->ptr = realloc(str->ptr, str->capacity * 2);
+		if (!str->ptr) {
+			fprintf(stderr, "couldn't allocate more memory.\n");
+			return -1;
+		}
+		str->capacity *= 2;
+	}
+
+	memcpy(str->ptr + str->len, c_str, strlen(c_str));
+	str->len += strlen(c_str) + 1;
+
+	*(str->ptr + str->len) = '\0';
+
+	return 0;
+}
+
+bz_str_t* bz_str_split(bz_str_t string, char delim) {
+
+	u_char *s = string.ptr;
+    bz_str_t* ans = malloc(sizeof(*ans)*10);
+	for (int i = 0, j = 0; i < string.len; i++) {
+        if( s[i] == delim ){
+
         }
-        str->capacity *= 2;
-
-    }
-
-    memcpy(str->ptr + str->len, c_str, strlen(c_str));
-    str->len += strlen(c_str) + 1;
-
-    *(str->ptr + str->len) = '\0';
-    
-    return 0;
+	}
 }
-
-int bz_str_split()
-
-
-
-
